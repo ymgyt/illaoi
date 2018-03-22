d1542cc44578f299ba65913d1d4e81e449b1801b
Yuta Yamaguchi <yamaguchi7073xtt@gmail.com>
2018-03-23 01:12:16 +0900

    [add] client ip/agent

--- main.go
+++ main.go
@@ -18,6 +18,14 @@ func main() {
 		c.String(200, time.Now().Format(time.RFC3339))
 	})
 
+	r.GET("/ip", func(c *gin.Context) {
+		c.String(200, c.ClientIP())
+	})
+
+	r.GET("/agent", func(c *gin.Context) {
+		c.String(200, c.GetHeader("User-Agent"))
+	})
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "I'm not big on sermons - broken bones teach better lessons",
